c3fe89c99f8a4da82980d2457925029f4a2ef35d
Alex Coupe <acoupe@gmail.com>
2020-03-19 20:00:12 +0000

    Starting work on locations

---
+++ app/Http/Resources/LocationResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LocationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        if ($this->resource != null)
+        {
+            return [
+                    "id" => $this->id,
+                    "name" => $this->name,
+                    "addressone" => $this->address_one,
+                    "addresstwo" => $this->address_two,
+                    "addressthree" => $this->address_three,
+                    "city" => $this->city,
+                    "state" => $this->state,
+                    "postcode" => $this->post_code,
+            ];
+        }
+    }
+}
