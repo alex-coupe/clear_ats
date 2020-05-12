485552e7043ccb81504e71fc7e9ed5526ed42a8d
Alex <acooupe@gmail.com>
2020-05-12 10:18:38 +0100

    started designing the job listing table

---
+++ database/migrations/2020_05_12_085146_create_job_listing_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobListingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_listing', function (Blueprint $table) {
+            $table->id();
+            $table->string('employer_ref');
+            $table->string('recruiter_ref')->nullable();
+            $table->string('job_title');
+            $table->string('remuneration');
+            $table->string('work_pattern');
+            $table->string('job_description');
+            $table->integer('employer_location_id');
+            $table->dateTime('expiry_date');
+            $table->dateTime('live_date')->nullable();
+            $table->string('job_description_url')->nullable();
+            $table->integer('recruiter_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_listing');
+    }
+}
